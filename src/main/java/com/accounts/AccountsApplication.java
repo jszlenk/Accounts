b8eb6793aff1ac72780264f546b6186a02a0112f
Kuba <kuba@mail.com>
2020-08-07 20:36:19 +0200

    Zadanie4

--- src/main/java/com/accounts/AccountsApplication.java
+++ src/main/java/com/accounts/AccountsApplication.java
@@ -7,11 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Optional;
 
 @Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountsApplication {
 
